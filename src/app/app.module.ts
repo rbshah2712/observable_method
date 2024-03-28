9ec1d8a0b1f29ff6dbc1fde4a0807369ce9b986b
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2024-03-28 12:47:09 -0400

    added solution for cypress issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrefreModule } from './prefre/prefre.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrefreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
