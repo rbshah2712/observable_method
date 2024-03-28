6be1f61d322fabe41b0ab9fe274d12101f11f1b4
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2024-03-27 20:54:39 -0400

    Added Testing and Cypress

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('observable_method app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Get Photos Using Album Id API');
   });
 });
